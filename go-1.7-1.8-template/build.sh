0b9a01a4828affb161a39dec3d7aaec0d1c2fb4c
Nick Miyake <nmiyake@palantir.com>
2017-04-11 10:28:14 -0700

    Bump Go 1.8 version to 1.8.1
    Also refactor build.sh logic to use variables for SHA-256
    checksums for Go distribution downloads.

--- go-1.7-1.8-template/build.sh
+++ go-1.7-1.8-template/build.sh
@@ -17,11 +17,11 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
 
 ENV GOLANG_1_7_VERSION {{GO_1_7_VERSION}}
 ENV GOLANG_1_7_DOWNLOAD_URL https://golang.org/dl/go$GOLANG_1_7_VERSION.linux-amd64.tar.gz
-ENV GOLANG_1_7_DOWNLOAD_SHA256 2e4dd6c44f0693bef4e7b46cc701513d74c3cc44f2419bf519d7868b12931ac3
+ENV GOLANG_1_7_DOWNLOAD_SHA256 {{GO_1_7_LINUX_SHA256}}
 
 ENV GOLANG_1_8_VERSION {{GO_1_8_VERSION}}
 ENV GOLANG_1_8_DOWNLOAD_URL https://golang.org/dl/go$GOLANG_1_8_VERSION.linux-amd64.tar.gz
-ENV GOLANG_1_8_DOWNLOAD_SHA256 53ab94104ee3923e228a2cb2116e5e462ad3ebaeea06ff04463479d7f12d27ca
+ENV GOLANG_1_8_DOWNLOAD_SHA256 {{GO_1_8_LINUX_SHA256}}
 
 RUN curl -fsSL "$GOLANG_1_7_DOWNLOAD_URL" -o golang.tar.gz \
   && echo "$GOLANG_1_7_DOWNLOAD_SHA256  golang.tar.gz" | sha256sum -c - \
@@ -47,7 +47,9 @@ COPY go-wrapper /usr/local/bin/'
 
 # replace variables in template with current versions
 dockerfile_content="${DOCKERFILE//\{\{GO_1_8_VERSION\}\}/$GO_1_8}"
+dockerfile_content="${dockerfile_content//\{\{GO_1_8_LINUX_SHA256\}\}/$GO_1_8_LINUX_AMD64_SHA256}"
 dockerfile_content="${dockerfile_content//\{\{GO_1_7_VERSION\}\}/$GO_1_7}"
+dockerfile_content="${dockerfile_content//\{\{GO_1_7_LINUX_SHA256\}\}/$GO_1_7_LINUX_AMD64_SHA256}"
 
 cd "$SCRIPT_DIR"
 imageDir="images/go-${GO_1_7}-${GO_1_8}"
